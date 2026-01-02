623c5cb066bf45160c0b225fbe1c62e29700de1a
Rahul Sugumar <rahulsugumar1977@gmail.com>
2026-01-02 23:06:54 +0530

    added backend hosted url

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
 });
-
 // Add a request interceptor to insert Custom User ID if present
 api.interceptors.request.use((config) => {
     const userId = localStorage.getItem('user_id');
